83be0df0adbfaab119f911cef1e15e89172d9b6a
Filipe Gonçalves <filipe@codinghighway.com>
2015-04-26 15:22:19 +0100

    Added question description for paths_sum

--- trees_graphs/paths_sum/solution.c
+++ trees_graphs/paths_sum/solution.c
@@ -1,3 +1,9 @@
+/* You are given a binary tree in which each node contains a value. Design an
+ * algorithm to print all paths which sum to a given value. The path does not need to
+ * start or end at the root or a leaf.
+ *
+ * Source: Cracking the Coding Interview, page 86, question 4.9
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
