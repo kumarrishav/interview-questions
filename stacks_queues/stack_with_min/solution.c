15576df15211c7658491362bdeb7137094a01110
Filipe Gonçalves <filipe@codinghighway.com>
2015-04-29 15:19:01 +0100

    Updated questions descriptions

--- stacks_queues/stack_with_min/solution.c
+++ stacks_queues/stack_with_min/solution.c
@@ -1,4 +1,9 @@
-
+/* How would you design a stack which, in addition to push and pop, also has a
+ * function min which returns the minimum element? Push, pop and min should
+ * all operate in O(1) time.
+ *
+ * Source: Cracking the Coding Interview, page 80, question 3.2
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
