5deb23af38524d5accaf91b4ac32f7f549cd1ae5
Filipe Gonçalves <filipe@codinghighway.com>
2015-04-29 17:10:20 +0100

    Updated questions descriptions

--- stacks_queues/sort_stack/solution.c
+++ stacks_queues/sort_stack/solution.c
@@ -1,4 +1,10 @@
-
+/* Write a program to sort a stack in ascending order (with biggest items on top).
+ * You may use at most one additional stack to hold items, but you may not copy
+ * the elements into any other data structure (such as an array). The stack supports
+ * the following operations: push, pop, peek, and isEmpty.
+ *
+ * Source: Cracking the Coding Interview, page 81, question 3.6
+ */
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
