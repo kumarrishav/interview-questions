5e418f10a2168fa6c30104f0e5b910c1796e5ddc
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-02 21:11:05 +0100

    Implemented closest match binary search

--- DS_Algos/bin_search/binary_search.c
+++ DS_Algos/bin_search/binary_search.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <assert.h>
 #define MAX_INPUT_SZ 256
+#define abs(x) ((x) >= 0 ? (x) : -(x))
 
 ssize_t binary_search(int arr[], size_t arr_sz, int val) {
 	ssize_t l = 0, r = arr_sz-1;
@@ -18,6 +20,35 @@ ssize_t binary_search(int arr[], size_t arr_sz, int val) {
 	}
 
 	return -1;
+}
+
+ssize_t bin_search_closest(int arr[], size_t arr_sz, int val) {
+	ssize_t l = 0, r = arr_sz-1;
+	ssize_t mid;
+
+	while (l <= r) {
+		mid = l+(r-l)/2;
+		if (arr[mid] == val) {
+			return mid;
+		}
+		if (arr[mid] < val) {
+			l = mid+1;
+		} else {
+			r = mid-1;
+		}
+	}
+
+	if (l < arr_sz && r >= 0) {
+		int diff_l = abs(val-arr[l]);
+		int diff_r = abs(val-arr[r]);
+		return diff_l < diff_r ? l : r;
+	} else if (l == arr_sz) {
+		return r;
+	} else if (r < 0) {
+		return l;
+	} else {
+		assert(0);
+	}
 }
 
 static int sorted_arr[MAX_INPUT_SZ];
@@ -41,12 +72,17 @@ int main(void) {
 			int val;
 			scanf("%d", &val);
 
+			printf("Traditional binary search: ");
 			ssize_t res = binary_search(sorted_arr, arr_sz, val);
 			if (res == -1) {
 				printf("%d not found\n", val);
 			} else {
 				printf("%d found at i = %zd\n", val, res);
 			}
+
+			printf("Closest match: ");
+			res = bin_search_closest(sorted_arr, arr_sz, val);
+			printf("%d\n\n", sorted_arr[res]);
 		}
 	}
 
