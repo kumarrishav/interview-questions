864c6b30072bd93d97df52755bc59391c6ceaec6
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-02 17:34:12 +0100

    Updated question description

--- arrays_strings/remove_chars/solution.c
+++ arrays_strings/remove_chars/solution.c
@@ -1,4 +1,13 @@
-
+/* Write an efficient function that deletes characters from an ASCII string. Use the prototype
+ * string removeChars(string str, string remove);
+ *
+ * where any character existing in `remove` must be deleted from `str`. For example,
+ * given a `str` of "Battle of the Vowels: Hawaii vs. Grozny" and a `remove` of "aeiou",
+ * the function should transform `str` to "Bttl f th Vwls: Hw vs. Grzny".
+ * Justify any design decisions you make, and discuss the efficiency of your solution.
+ *
+ * Source: Programming Interviews Exposed, page 88
+ */
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
