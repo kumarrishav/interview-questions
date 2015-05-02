d1f4a1f60bc0dd431fcd52b03cb90ee19d40ea8f
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-02 19:11:30 +0100

    Implemented int to char * and char * to int conversion

---
+++ arrays_strings/int_conversion/solution.c
@@ -0,0 +1,92 @@
+
+#include <stdio.h>
+
+// Assumption: 0-9 are consecutive on the platform's encoding
+int char_to_digit(char c) {
+	return c - '0';
+}
+
+char digit_to_char(int d) {
+	return d+'0';
+}
+
+// Assumption: str is well formatted and represents an `int` that is not out of range
+int str_to_int(const char *str) {
+	size_t curr = 0;
+
+	int res = 0;
+	int sign = 1;
+
+	if (str[curr] == '-') {
+		sign = -1;
+		curr++;
+	}
+
+	while (str[curr] != '\0') {
+		res = res*10+char_to_digit(str[curr]);
+		curr++;
+	}
+
+	return res*sign;
+}
+
+void reverse_str(char *, char *);
+void int_to_str(int n, char *res) {
+
+	if (n == 0) {
+		*res++ = '0';
+		*res = '\0';
+		return;
+	}
+
+	size_t i = 0;
+	int sign = 1;
+
+	if (n < 0) {
+		sign = -1;
+	}
+
+	while (n != 0) {
+		res[i++] = digit_to_char(sign*(n%10));
+		n /= 10;
+	}
+
+	if (sign == -1) {
+		res[i++] = '-';
+	}
+
+	res[i] = '\0';
+	reverse_str(res, res+i);
+}
+
+void reverse_str(char *str, char *end) {
+	if (str == end) {
+		return;
+	}
+
+	end--;
+
+	while (str < end) {
+		char tmp = *str;
+		*str = *end;
+		*end = tmp;
+		str++;
+		end--;
+	}
+}
+
+static char input[32];
+
+int main(void) {
+	printf("Enter a valid integer as a string\n");
+
+	while (scanf("%s", input) == 1) {
+		printf("Received: %s\n", input);
+		int converted = str_to_int(input);
+		printf("`char *` -> `int`: %d\n", converted);
+		int_to_str(converted, input);
+		printf("`int` -> `char *`: %s\n", input);
+	}
+
+	return 0;
+}
