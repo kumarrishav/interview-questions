5d9a5e371d6d6da0d59fa0b229a06ce4a68ac853
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-02 23:39:00 +0100

    Implemented dynamic programming for the stairs problem

--- recursion/stairs/solution.c
+++ recursion/stairs/solution.c
@@ -1,15 +1,31 @@
 
 #include <stdio.h>
+#include <string.h>
 
-unsigned long long possibilities(int left) {
+/* Without dynamic programming, this would be O(3^N) - terrible! */
+unsigned long long possibilities_aux(int left, unsigned long long cached[]) {
 	if (left < 0) {
 		return 0;
 	}
 	if (left == 0) {
 		return 1;
 	}
+	if (cached[left-1] != 0) {
+		return cached[left-1];
+	} else {
+		cached[left-1] =
+			possibilities_aux(left-1, cached) +
+			possibilities_aux(left-2, cached) +
+			possibilities_aux(left-3, cached);
+	}
 
-	return possibilities(left-1)+possibilities(left-2)+possibilities(left-3);
+	return cached[left-1];
+}
+
+unsigned long long possibilities(int stairs) {
+	unsigned long long cached[stairs];
+	memset(cached, 0, sizeof(cached));
+	return possibilities_aux(stairs, cached);
 }
 
 int main(void) {
