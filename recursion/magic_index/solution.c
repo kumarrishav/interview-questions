6e168fcd28ea50a61b965823b7006d569ceba245
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-03 16:21:49 +0100

    Updated magic index question description

--- recursion/magic_index/solution.c
+++ recursion/magic_index/solution.c
@@ -1,4 +1,9 @@
-
+/* A magic index in an array A[0..n-1] is defined to be an index such that A[i] = i.
+ * Given a sorted array of distinct integers, write a method to find a magic
+ * index, if one exists, in array A.
+ *
+ * Source: Cracking the Coding Interview, page 109, question 9.3
+ */
 #include <stdio.h>
 
 ssize_t magic_index(int arr[], size_t arr_sz) {
