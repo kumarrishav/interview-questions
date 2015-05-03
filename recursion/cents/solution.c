fbb671aea7e7954fa6c07ad1f8753e6928248000
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-03 23:35:38 +0100

    Fixed bug in the cents solution

--- recursion/cents/solution.c
+++ recursion/cents/solution.c
@@ -1,15 +1,38 @@
 
 #include <stdio.h>
 
-unsigned ways(int n) {
-	if (n < 0) {
-		return 0;
-	}
+static unsigned quantities[] = { 25, 10, 5, 1 };
+static size_t quantities_sz = sizeof(quantities)/sizeof(quantities[0]);
+
+unsigned ways_aux(size_t next_qty, int n) {
 	if (n == 0) {
 		return 1;
 	}
+	if (next_qty == quantities_sz) {
+		return 0;
+	}
 
-	return ways(n-25)+ways(n-10)+ways(n-5)+ways(n-1);
+	unsigned res = 0;
+	unsigned i;
+	for (i = 0; i*quantities[next_qty] <= n; i++) {
+		res += ways_aux(next_qty+1, n-i*quantities[next_qty]);
+	}
+
+	return res;
+}
+
+unsigned ways(int n) {
+	size_t first_qty;
+	if (n >= 25) {
+		first_qty = 0;
+	} else if (n >= 10) {
+		first_qty = 1;
+	} else if (n >= 5) {
+		first_qty = 2;
+	} else {
+		first_qty = 3;
+	}
+	return ways_aux(first_qty, n);
 }
 
 int main(void) {
