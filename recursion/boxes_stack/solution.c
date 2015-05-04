6cde6d7b309678bd3dbdf463c84a43d169dca962
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-04 22:18:08 +0100

    Updated question description for stack of boxes problem

--- recursion/boxes_stack/solution.c
+++ recursion/boxes_stack/solution.c
@@ -1,5 +1,11 @@
-
-
+/* You have a stack of n boxes, with widths wi, heights hi, and depths di. The boxes
+ * cannot be rotated and can only be stacked on top of one another if each box
+ * in the stack is strictly larger than the box above it in width, height and depth.
+ * Implement a method to build the tallest stack possible, where the height of a 
+ * stack is the sum of the heights of each box.
+ *
+ * Source: Cracking the Coding Interview, page 110, question 9.10
+ */
 #include <stdio.h>
 
 /* Note: We only return the height of the tallest stack possible.
