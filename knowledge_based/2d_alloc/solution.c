854d3d73b9b5fb1cd1d95883ff5d91d8c8a4078f
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-07 19:08:41 +0100

    Updated question description for the 2D alloc problem

--- knowledge_based/2d_alloc/solution.c
+++ knowledge_based/2d_alloc/solution.c
@@ -1,4 +1,9 @@
-
+/* Write a function in C called my2DAlloc which allocates a two-dimensional
+ * array. Minimize the number of calls to malloc and make sure that the memory
+ * is accessible by the notation arr[i][j].
+ *
+ * Source: Cracking the Coding Interview, page 140, quetion 13.10
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
