712dfae3f1c585b971df89e50e847809427a424f
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-07 23:36:14 +0100

    Updated "Telephone Words" question description

--- recursion/telephone_words/solution.c
+++ recursion/telephone_words/solution.c
@@ -1,9 +1,55 @@
-
+/* People in the United States often give others their telephone number
+ * as a word representing the seven-digit number after the area code. For
+ * example, if my telephone number were 866-2665, I could tell people my number
+ * is "TOOCOOL", instead of the hard-to-remember seven-digit number. Note
+ * that many other possibilities (most of which are nonsensical) can represent
+ * 866-2665. You can see how letters correspond to numbers on a telephone keypad in
+ * Figure 7-2.
+ *
+ *  ~~~ ~~~ ~~~
+ * | 1 | 2 | 3 |
+ * |   |ABC|DEF|
+ *  ~~~ ~~~ ~~~
+ * | 4 | 5 | 6 |
+ * |GHI|JKL|MNO|
+ *  ~~~ ~~~ ~~~
+ * | 7 | 8 | 9 |
+ * |PRS|TUV|WXY|
+ *  ~~~ ~~~ ~~~
+ * | 4 | 5 | 6 |
+ * | * | 0 | # |
+ *  ~~~ ~~~ ~~~
+ *
+ * Write a function that takes a seven-digit telephone number and prints out all of
+ * the possible "words" or combinations of letters that can represent the given number.
+ * Because the 0 and 1 keys have no letters on them, you should change only the digits
+ * 2-9 to letters. You'll be passed an array of seven integers, with each element
+ * being one digit in the number. You may assume that only valid phone numbers
+ * will be passed to your function. You can use the helper function
+ *
+ * char getCharKey(int telephoneKey, int place)
+ *
+ * which takes a telephone key (0-9) and a place of either 1, 2, 3 and returns the
+ * character corresponding to the letter in that position on the specified key. For
+ * example, GetCharKey(3, 2) will return 'E' because the telephone key 3 has the
+ * letters "DEF" on it and 'E' is the second letter.
+ *
+ * Source: Programming Interviews Exposed, page 119
+ */
 #include <stdio.h>
 #include <ctype.h>
 #include <assert.h>
 #define NUMBER_LEN 7
 
+/* Note:
+ * This function does not accurately emulate a phone terminal for keys >= 7
+ * This is because 7 maps to PRS, and the function assumes that each key >= 2
+ * maps to the 3 next letters in the alphabet (so, it assumes 7 maps to PQR,
+ * 8 to STU, and 9 to VWX).
+ * Thus, for keys >= 7, it returns the wrong letters. But this is not really relevant
+ * for the algorithm. I just implemented it this way for convenience, and because it
+ * makes it easy to come up with a general formula, instead of hardcoding everything.
+ */
 char get_char_key(int telephone_key, int place) {
 	assert(2 <= telephone_key && telephone_key <= 9);
 	assert(1 <= place && place <= 3);
@@ -53,7 +99,7 @@ static int read_number(int number[]) {
 
 int main(void) {
 
-	printf("Enter 7 digits representing a US phone number (without area code)\n");
+	printf("Enter 7 consecutive digits representing a US phone number (without area code)\n");
 	printf("> ");
 
 	int phone_number[NUMBER_LEN];
