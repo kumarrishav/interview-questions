b54b0d136f0b5b7c949c4678f92f2db1c5178377
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-08 23:45:08 +0100

    Updated "count ones" problem description

--- bit_manipulation/count_ones/solution.c
+++ bit_manipulation/count_ones/solution.c
@@ -1,4 +1,8 @@
-
+/* Write a function that determines the number of 1 bits in the binary
+ * representation of a given integer.
+ *
+ * Source: Programming Interviews Exposed, page 203
+ */
 #include <stdio.h>
 
 unsigned count_ones(unsigned int n) {
