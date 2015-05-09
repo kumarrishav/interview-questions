e36dad12d88baf96349431902da64a27ef1ca384
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-09 23:12:27 +0100

    Implemented same_page() method

---
+++ bit_manipulation/same_page/solution.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#define PAGE_SIZE 4096
+#define PAGE_SIZE_MASK (~((uintptr_t) (PAGE_SIZE-1)))
+
+int same_page(void *ptr1, void *ptr2) {
+	return (((uintptr_t) ptr1) & PAGE_SIZE_MASK) == (((uintptr_t) ptr2) & PAGE_SIZE_MASK);
+}
+
+int main(void) {
+	printf("Page size = %d; Pointers are %zu bits\n", PAGE_SIZE, sizeof(void *)*CHAR_BIT);
+	printf("Enter 2 memory addresses to see if they are on the same memory page\n");
+	printf("> ");
+
+	void *ptr1, *ptr2;
+	while (scanf("%p%p", &ptr1, &ptr2) == 2) {
+		if (same_page(ptr1, ptr2)) {
+			printf("They are on the same page\n");
+		} else {
+			printf("They are not on the same page\n");
+		}
+		printf("> ");
+	}
+
+	return 0;
+}
