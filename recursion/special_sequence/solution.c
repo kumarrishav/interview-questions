f105a0dcb86602abd9ea94bf0b0ad5b37e9c5fd2
Filipe Goncalves <filipe@codinghighway.com>
2015-05-11 18:09:15 +0100

    Updated "special sequences" problem description

--- recursion/special_sequence/solution.c
+++ recursion/special_sequence/solution.c
@@ -1,5 +1,21 @@
-
-
+/* A special sequence is a sequence where no two adjacent numbers are the same,
+ * and it only uses the numbers 1, 2, 3 and 4.
+ *
+ * We are interested in finding every special sequence with a given number of 1s, 2s,
+ * 3s and 4s. Let n1 be the desired number of 1s, n2 the desired number of 2s, etc.
+ *
+ * Write a method that, given n1, n2, n3 and n4, returns how many special sequences exist.
+ *
+ * EXAMPLE
+ * If n1 = n2 = n3 = n4, we are looking for special sequences that have a 1, a 2 a 3 and a 4.
+ * There are 24 of these:
+ * 1, 2, 3, 4
+ * 1, 2, 4, 3
+ * 1, 4, 2, 3
+ * ...
+ *
+ * Source: Careercup (Google interview)
+ */
 #include <stdio.h>
 
 unsigned generate_sequences_aux(unsigned available[4], unsigned still_usable, unsigned last) {
