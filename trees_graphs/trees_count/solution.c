f0ffd9a07eb86dcf05d93582972ee6a3996eb559
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-12 15:44:25 +0100

    Updated "trees count" problem description

--- trees_graphs/trees_count/solution.c
+++ trees_graphs/trees_count/solution.c
@@ -1,4 +1,10 @@
-
+/* Write a method that calculates the number of binary trees of size N.
+ *
+ * FOLLOW UP
+ * What if we want the number of binary search trees of size N?
+ *
+ * Source: past interview experience
+ */
 #include <stdio.h>
 
 unsigned long long trees(unsigned n) {
