484b856419b41612a178a29e2492500030f8d7d7
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-12 19:20:08 +0100

    Solved the "duplicates within k" problem

---
+++ arrays_strings/duplicates_within_k/solution.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+bool duplicates_within_k(const vector<int> &array, vector<int>::size_type k) {
+	if (k+1 > array.size()) {
+		return false;
+	}
+
+	set<int> window;
+
+	for (vector<int>::size_type i = 0; i < k+1; i++) {
+		if (!window.insert(array[i]).second) {
+			return true;
+		}
+	}
+
+	for (vector<int>::size_type i = k+1; i < array.size(); i++) {
+		window.erase(array[i-k-1]);
+		if (!window.insert(array[i]).second) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int main(void) {
+	cout << "Enter the array size, followed by K, followed by the array elements" << endl;
+	cout << "> ";
+
+	vector<int>::size_type array_sz;
+	vector<int>::size_type k;
+	while (cin >> array_sz >> k) {
+		vector<int> array;
+		for (vector<int>::size_type i = 0; i < array_sz; i++) {
+			int val;
+			cin >> val;
+			array.push_back(val);
+		}
+
+		if (duplicates_within_k(array, k)) {
+			cout << "true" << endl;
+		} else {
+			cout << "false" << endl;
+		}
+
+		cout << "> ";
+	}
+
+	return 0;
+}
