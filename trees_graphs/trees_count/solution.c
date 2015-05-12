dfdf8a82bb298651658c01f37f78c184f9324fa8
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-12 15:43:03 +0100

    Solved "trees count" follow up question

--- trees_graphs/trees_count/solution.c
+++ trees_graphs/trees_count/solution.c
@@ -9,19 +9,34 @@ unsigned long long trees(unsigned n) {
 	unsigned long long res = 0;
 	unsigned i;
 	for (i = 0; i < n; i++) {
-		res = res + 2*trees(i)*trees(n-i-1);
+		res += 2*trees(i)*trees(n-i-1);
+	}
+
+	return res;
+}
+
+unsigned long long bsts(unsigned n) {
+	if (n <= 1) {
+		return 1;
+	}
+
+	unsigned long long res = 0;
+	unsigned i;
+	for (i = 0; i < n; i++) {
+		res += bsts(i)*bsts(n-i-1);
 	}
 
 	return res;
 }
 
 int main(void) {
-	printf("Enter a positive integer to calculate the number of binary trees of size N\n");
+	printf("Enter a positive integer to calculate the number of binary trees of size N\n"
+	       "and the number of binary search trees of size N\n");
 	printf("> ");
 
 	unsigned n;
 	while (scanf("%u", &n) == 1) {
-		printf("%llu\n", trees(n));
+		printf("Binary trees: %llu\nBinary search trees: %llu\n", trees(n), bsts(n));
 		printf("> ");
 	}
 
