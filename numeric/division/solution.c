5253ebf440eca9d13c00d20ec47a49b46979af95
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-13 16:22:03 +0100

    Updated "division" question description

--- numeric/division/solution.c
+++ numeric/division/solution.c
@@ -1,4 +1,9 @@
-
+/* Write a function which, given two integers (a numerator and a denominator), prints the first
+ * N digits of a rational number. For example, for 5 / 3 with N=5, the result should be "1.66666".
+ * With N = 2, the result is 1.66
+ *
+ * Source: Careercup (Google interview)
+ */
 #include <stdio.h>
 
 void divide(int numerator, int denominator, int n) {
