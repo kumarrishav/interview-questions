f6bb3a2c2f9a701a0b68d5e220be9b36c19786c1
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-20 16:50:56 +0100

    Solved the "target number" problem using recursion

---
+++ recursion/target_number/solution.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+#include <string.h>
+
+unsigned target_n_aux(size_t n, int numbers[n], size_t i, int res_so_far, int target,
+		      char *buff, size_t buff_i) {
+	if (i == n-1) {
+		if (res_so_far == target) {
+			printf("%s%d\n", buff, numbers[i]);
+			return 1;
+		}
+		return 0;
+	}
+
+	unsigned res = 0;
+
+	buff_i += sprintf(&buff[buff_i], "%d+", numbers[i]);
+	res += target_n_aux(n, numbers, i+1, res_so_far+numbers[i+1], target, buff, buff_i);
+	buff[buff_i-1] = '-';
+	res += target_n_aux(n, numbers, i+1, res_so_far-numbers[i+1], target, buff, buff_i);
+
+	return res;
+}
+
+unsigned target_n(size_t n, int numbers[n], int target) {
+	if (n == 0) {
+		return 0;
+	}
+
+	char buff[n*32];
+	memset(buff, 0, sizeof(buff));
+
+	return target_n_aux(n, numbers, 0, numbers[0], target, buff, 0);
+}
+
+int main(void) {
+	printf("Enter the array size, followed by the array elements, followed by the target value.\n");
+	printf("> ");
+
+	size_t arr_sz;
+	while (scanf("%zu", &arr_sz) == 1) {
+
+		int numbers[arr_sz];
+
+		size_t i;
+		for (i = 0; i < arr_sz; i++) {
+			scanf("%d", &numbers[i]);
+		}
+
+		int target;
+		scanf("%d", &target);
+
+		if (target_n(arr_sz, numbers, target) == 0) {
+			printf("Impossible.\n");
+		}
+
+		printf("> ");
+	}
+
+	return 0;
+}
