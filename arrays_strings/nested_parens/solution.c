979ae4ad34f55af9d631f2dc34f007ebedce8605
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-05 18:03:13 +0100

    Solved "nested parentheses"

---
+++ arrays_strings/nested_parens/solution.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <assert.h>
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+unsigned max_nest_depth(const char *str) {
+	unsigned res = 0;
+	unsigned opened = 0;
+
+	size_t i;
+	for (i = 0; str[i] != '\0'; i++) {
+		if (str[i] == '(') {
+			opened++;
+			res = max(res, opened);
+		} else if (str[i] == ')') {
+			assert(opened > 0);
+			opened--;
+		}
+	}
+
+	assert(opened == 0);
+
+	return res;
+}
+
+static char buf[512];
+
+int main(void) {
+	printf("Enter a string with parentheses to count max nesting level.\n");
+	printf("> ");
+
+	while (scanf("%s", buf) == 1) {
+		printf("Max nesting level = %d\n> ", max_nest_depth(buf));
+	}
+
+	return 0;
+}
