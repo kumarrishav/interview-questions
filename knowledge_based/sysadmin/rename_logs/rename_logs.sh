405e4e263bbc72ef9a9ec50f43ef1bbd1c0d9786
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-10 00:09:32 +0100

    Implemented "rename logs"

--- knowledge_based/sysadmin/rename_logs/rename_logs.sh
+++ knowledge_based/sysadmin/rename_logs/rename_logs.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-find . -name '*.log' | while read fname; do echo mv $fname ${fname/.log/.LOG}; done | bash -x
+# You are in a directory that has numerous log files scattered all over the place, including
+# sub-directories.
+# The log files names are not uniform: some end with .log, others with .lOg, others with .LOG,
+# and other variations.
+# Write a script that finds all of these files and standardizes the names such that all of them
+# end with .log
+
+find . -iname '*.log' | while read fname; do newname=`echo $fname | sed 's/\.log/.log/i'`; if [ $fname != $newname ]; then echo mv $fname $newname; fi; done | bash -x
