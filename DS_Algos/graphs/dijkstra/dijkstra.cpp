22785931f496182e8b750239bd5e9dacccb489dd
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-13 15:25:26 +0100

    Implemented Dijkstra's algorithm

--- DS_Algos/graphs/dijkstra/dijkstra.cpp
+++ DS_Algos/graphs/dijkstra/dijkstra.cpp
@@ -1,119 +1,15 @@
+/* Dijkstra's algorithm implementation */
+
 #include <algorithm>
 #include <vector>
 #include <iostream>
 #include <memory>
 #include <cassert>
 #include <map>
+#include <set>
+#include <limits>
 
 using namespace std;
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * ~~~~~~~~~~  Priority Queue ~~~~~~~~~~
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *
- * This is a priority queue implementation based on a min-heap
- *
- * Insertion: O(log(n))
- * Deletion: O(log(n))
- * Peek: O(1)
- * IsEmpty: O(1)
- * Size: O(1)
- */
-
-template<class T>
-class PriorityQueue {
-private:
-	vector<T> min_heap;
-
-	void sift_down(void) {
-		typename vector<T>::size_type curr = 0;
-		typename vector<T>::size_type left = curr*2+1;
-		typename vector<T>::size_type right = left+1;
-		typename vector<T>::size_type smallest = curr;
-
-		if (left < min_heap.size() && min_heap[left] < min_heap[smallest]) {
-			smallest = left;
-		}
-		if (right < min_heap.size() && min_heap[right] < min_heap[smallest]) {
-			smallest = right;
-		}
-
-		while (curr != smallest) {
-			swap(min_heap[curr], min_heap[smallest]);
-			curr = smallest;
-			left = curr*2+1;
-			right = left+1;
-
-			if (left < min_heap.size() && min_heap[left] < min_heap[smallest]) {
-				smallest = left;
-			}
-			if (right < min_heap.size() && min_heap[right] < min_heap[smallest]) {
-				smallest = right;
-			}
-
-		}
-	}
-
-	void sift_up(void) {
-		typename vector<T>::size_type curr = min_heap.size()-1;
-		typename vector<T>::size_type parent = (curr+1)/2-1;
-
-		while (curr != 0 && min_heap[curr] < min_heap[parent]) {
-			swap(min_heap[curr], min_heap[parent]);
-			curr = parent;
-			parent = (curr+1)/2-1;
-		}
-
-	}
-
-	T extract_min(void) {
-		swap(min_heap[0], min_heap[min_heap.size()-1]);
-		T ret = min_heap[min_heap.size()-1];
-		min_heap.pop_back();
-		sift_down();
-		return ret;
-	}
-
-	void insert(const T &element) {
-		min_heap.push_back(element);
-		sift_up();
-	}
-
-	T peek_min(void) {
-		return min_heap[0];
-	}
-
-public:
-
-	typedef typename vector<T>::size_type size_type;
-
-	PriorityQueue(void) { }
-
-	T dequeue(void) {
-		return extract_min();
-	}
-
-	void enqueue(const T &element) {
-		insert(element);
-	}
-
-	T peek(void) {
-		return peek_min();
-	}
-
-	bool empty(void) {
-		return min_heap.size() == 0;
-	}
-
-	size_type size(void) {
-		return min_heap.size();
-	}
-};
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * ~~~~~~~~~~  End of Priority Queue ~~~~~~~~~~
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * ~~~~~~~~~~  Graph ~~~~~~~~~~
@@ -129,6 +25,9 @@ struct graph_node {
 	int id;
 	vector<graph_edge> neighbors;
 	bool visited;
+	unsigned distance; // Used by Dijkstra
+	graph_node *prev; // Used by Dijkstra
+
 	graph_node(void) { }
 	graph_node(int identifier) : id(identifier) { }
 };
@@ -140,6 +39,16 @@ struct graph_edge {
 	graph_edge(void) { }
 };
 
+class DistComparator {
+public:
+	/* Sets cannot have more than one element with the same key,
+	 * so we need to use the node ID to untie
+	 */
+	bool operator()(graph_node *a, graph_node *b) {
+		return (a->distance < b->distance) || (a->distance == b->distance && a->id < b->id);
+	}
+};
+
 class Graph {
 private:
 	map<int, graph_node> nodes;
@@ -147,9 +56,32 @@ private:
 	void dfs_visit(graph_node *node) {
 		node->visited = true;
 		cout << node->id << endl;
-		for (vector<graph_edge>::iterator child_it = node->neighbors.begin(); child_it != node->neighbors.end(); child_it++) {
+		for (vector<graph_edge>::iterator child_it = node->neighbors.begin();
+		     child_it != node->neighbors.end();
+		     child_it++) {
 			if (!child_it->target->visited) {
 				dfs_visit(child_it->target);
+			}
+		}
+	}
+
+	void print_path(const graph_node *src, const graph_node *curr) {
+		if (curr == src) {
+			cout << src->id;
+			return;
+		}
+		print_path(src, curr->prev);
+		cout << " -> " << curr->id;
+	}
+
+	void print_shortest_paths(int source) {
+		graph_node *src = &nodes[source];
+		for (map<int, graph_node>::const_iterator node_it = nodes.begin();
+		     node_it != nodes.end();
+		     node_it++) {
+			if (node_it->second.prev != NULL) {
+				print_path(src, &node_it->second);
+				cout << endl;
 			}
 		}
 	}
@@ -170,14 +102,64 @@ public:
 	}
 
 	void dfs(void) {
-		for (map<int, graph_node>::iterator node_it = nodes.begin(); node_it != nodes.end(); node_it++) {
+		for (map<int, graph_node>::iterator node_it = nodes.begin();
+		     node_it != nodes.end();
+		     node_it++) {
 			node_it->second.visited = false;
 		}
-		for (map<int, graph_node>::iterator node_it = nodes.begin(); node_it != nodes.end(); node_it++) {
+		for (map<int, graph_node>::iterator node_it = nodes.begin();
+		     node_it != nodes.end();
+		     node_it++) {
 			if (!node_it->second.visited) {
 				dfs_visit(&node_it->second);
 			}
 		}
+	}
+
+	void dijkstra(int source) {
+		set<graph_node*, DistComparator> queue;
+
+		for (map<int, graph_node>::iterator node_it = nodes.begin();
+		     node_it != nodes.end();
+		     node_it++) {
+			node_it->second.prev = NULL;
+			if (node_it->second.id == source) {
+				node_it->second.distance = 0;
+			} else {
+				node_it->second.distance = numeric_limits<unsigned>::max();
+			}
+			queue.insert(&node_it->second);
+		}
+
+		while (!queue.empty()) {
+			graph_node *n = *queue.begin();
+			queue.erase(queue.begin());
+			for (vector<graph_edge>::iterator edge_it = n->neighbors.begin();
+			     edge_it != n->neighbors.end();
+			     edge_it++)  {
+
+				set<graph_node*, DistComparator>::iterator target_it;
+				target_it = queue.find(edge_it->target);
+
+				/* We test n->distance != UINT_MAX because UINT_MAX
+				 * simulates infinity, and x+infinity == infinity
+				 * (i.e. we don't want to wrap around on unreachable nodes)
+				 */
+
+				if (target_it != queue.end() &&
+				    n->distance != numeric_limits<unsigned>::max() &&
+				    n->distance+edge_it->cost < edge_it->target->distance) {
+
+					queue.erase(target_it);
+					edge_it->target->distance = n->distance+edge_it->cost;
+					edge_it->target->prev = n;
+					queue.insert(edge_it->target);
+
+				}
+			}
+		}
+
+		print_shortest_paths(source);
 	}
 
 	size_type size(void) {
@@ -189,6 +171,7 @@ int main(void) {
 
 	cout << "Enter number of nodes, followed by each node id, followed by:" << endl;
 	cout << "id number_of_neighbors neighbor1_id cost_to_neighbor1 neighbor2_id cost_to_neighbor2 ... neighborN_id cost_to_neighborN" << endl;
+	cout << "Followed by the source" << endl;
 	cout << "> ";
 
 	Graph::size_type nodes;
@@ -214,7 +197,14 @@ int main(void) {
 			}
 		}
 
+		int src;
+		cin >> src;
+
+		cout << "Performing a DFS visit" << endl;
 		graph.dfs();
+
+		cout << "Calculating shortest paths from " << src << endl;
+		graph.dijkstra(src);
 
 		cout << "> ";
 	}
