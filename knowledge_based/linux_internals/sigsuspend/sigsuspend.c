a67ad49797d2ab83c183013509b46e625d707e74
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-16 22:04:26 +0100

    Added explanation about sigsuspend(2)

--- knowledge_based/linux_internals/sigsuspend/sigsuspend.c
+++ knowledge_based/linux_internals/sigsuspend/sigsuspend.c
@@ -11,6 +11,41 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* pause(2) puts the process to sleep until a signal is delivered that either terminates the process
+ * or causes the invocation of a signal-catching function, whereas sigsuspend(2) atomically sets a
+ * process's signal mask and sleeps until a signal is delivered that either terminates the process
+ * or causes the invocation of a signal-catching function.
+ *
+ * The difference is subtle, but important. sigsuspend(2) avoids race conditions between unblocking
+ * a signal and calling pause(2). Consider this code sample:
+ *
+ * ...
+ * sigset_t unblock;
+ * sigemptyset(&unblock);
+ * sigprocmask(SIG_SETMASK, &unblock, NULL); // Unblock every signal
+ * pause(); // Sleep until a signal is delivered
+ *
+ * The problem with the above snippet is that there is a window of time between the call to
+ * sigprocmask(2) and the call to pause(2) where the signal may be raised and delivered.
+ * Assuming that the signal will never be raised again, pause(2) will sleep forever, whereas
+ * in reality the signal already occurred. This is one of those examples where the code seems right
+ * and mostly works, but sometimes things go wrong. Problems like this are very hard to debug.
+ *
+ * sigsuspend(2) solves this problem by atomically changing the signal mask and waiting for
+ * a signal. sigsuspend(2) also has the added benefit of restoring the signal mask once it returns.
+ *
+ * The code below shows a case where pause(2) misses a signal. To see it running, compile without
+ * -DUSE_SIGSUSPEND. It works by blocking SIGINT and setting up a handler for SIGINT, followed by
+ * raise(SIGINT), which causes SIGINT to be generated for the running process. Then, the original
+ * signal mask is restored, unblocking SIGINT. sigprocmask(2) returns only after pending signals
+ * are delivered and their handlers executed, so by the time pause(2) is called, the signal has
+ * already been lost. The process sleeps forever.
+ *
+ * Compiling with -DUSE_SIGSUSPEND will illustrate the use of sigsuspend(2). Because sigsuspend(2)
+ * atomically sets the signal mask and waits for a signal, the process does not sleep forever
+ * anymore.
+ */
+
 void sigint(int signo) {
 	printf("Caught SIGINT\n");
 }
