99f6d2418076c1ac4246c8b957b900d4766036a5
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-17 22:15:40 +0100

    Updated "plus minus 1" description

--- arrays_strings/plus_minus_1/solution.c
+++ arrays_strings/plus_minus_1/solution.c
@@ -1,7 +1,23 @@
-
+/* You are given an array consisting of elements where A[i+1] == A[i]+1 || A[i+1] == A[i]-1
+ *
+ * Write a function to find a given element in the array.
+ *
+ * EXAMPLE
+ * Input: find 6 in [ 1, 2, 3, 4, 3, 4, 5, 6, 7 ]
+ * Output: 7 (A[7] == 6)
+ *
+ * Source: Careercup (Amazon interview)
+ */
 #include <stdio.h>
 #define abs(n) ((n) < 0 ? -(n) : (n))
 
+/* The trick here is to skip positions where it would be impossible to find the given value.
+ * For the above example, if we start at i = 0, we can see that we need 6-1 = 5 increments
+ * In the best case, these 5 increments take place in the next 5 positions, so if A[0] == 1,
+ * 6 can't appear before A[5].
+ *
+ * We keep doing this iteratively
+ */
 ssize_t find_elem(int arr[], size_t arr_sz, int val) {
 	size_t i = 0;
 
