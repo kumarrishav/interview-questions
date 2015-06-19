6e3c499fd6077976e217f617c357f87df99c72f3
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-19 16:27:22 +0100

    Added explanation for LIS algorithm based on patience sorting

--- DS_Algos/LIS/optimized1.cpp
+++ DS_Algos/LIS/optimized1.cpp
@@ -1,4 +1,22 @@
-
+/* An O(N log(N)) algorithm to find the Longest Increasing Subsequence using a
+ * modified Patience Sorting.
+ *
+ * The algorithm works as follows: first, run a modified patience sorting to create the piles.
+ * The modified version keeps track of paths along the piles as new nodes are inserted: when a node
+ * is pushed into pile `i`, that node will store a pointer to the top node of pile `i-1`.
+ *
+ * When patience sort ends, the size of the Longest Increasing Subsequence is equal to the number of
+ * piles. To reconstruct the subsequence, start from the top node in the last pile, and follow the
+ * pointers backwards until the first pile. The elements traversed constitude the Longest Increasing
+ * Subsequence. When traversing this path, push each element into the output result array, and then
+ * reverse the array.
+ *
+ * Note that a naive linear scan along the piles when performing patience sorting blows the
+ * O(N log(N)) bound. Instead of doing this, since the top elements of each pile are
+ * in increasing order from left to right, we perform binary search at each step to find
+ * the leftmost pile such that its top is greater than the node we want to insert, thus bringing
+ * the complexity to the desired O(N log(N)) bound.
+ */
 #include <iostream>
 #include <vector>
 #include <algorithm>
