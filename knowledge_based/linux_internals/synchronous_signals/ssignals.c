edc30fb124eb5f1a9410c757e67e3dd4030ab59e
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-22 23:42:05 +0100

    Updated synchronous signals description

--- knowledge_based/linux_internals/synchronous_signals/ssignals.c
+++ knowledge_based/linux_internals/synchronous_signals/ssignals.c
@@ -1,3 +1,27 @@
+/* This program illustrates how to deal with signals synchronously.
+ * It works by creating a thread that is responsible for handling signals.
+ *
+ * The main thread is notified by the signal handler thread with condition variables,
+ * which essentially turns the underlying asynchronous signal handling environment into a
+ * synchronous programming environment, which is wonderful.
+ *
+ * A few points worth mentioning:
+ *
+ * Using sigprocmask(2) in multithreaded code leads to undefined behavior. Each thread has its own
+ * signal mask (although the signals disposition is shared among all threads), and to manipulate it,
+ * pthread_sigmask(3) must be used instead.
+ *
+ * Similarly, multithreaded code cannot use `sigsuspend(2)`. It must use the thread-equivalent
+ * sigwait(3). It is not mentioned in the manpage, but to avoid erroneous behavior, the thread must
+ * block the signals to wait for before calling sigwait(3) with pthread_sigmask(3).
+ * sigwait(3) will atomically unblock the signals and suspend execution until one of the specified
+ * signals is delivered to the thread.
+ *
+ * The advantage of having a thread dealing with signals is that the thread handles the signal in
+ * a regular function, outside of the context of a signal handler, and as such it can call any
+ * functions, even if they are not async-signal safe.
+ *
+ */
 
 #include <stdio.h>
 #include <signal.h>
