0b9544a9beacb6ae8c147f345c8731c4877e2824
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-23 15:18:30 +0100

    Simplified 2D binary search code.

--- sorting_searching/2d_binsearch/solution2.c
+++ sorting_searching/2d_binsearch/solution2.c
@@ -26,52 +26,39 @@ struct cell {
 	ssize_t col;
 };
 
-struct cell bin_search_2d_aux(size_t M, size_t N, int matrix[M][N],
-			  ssize_t row_l, ssize_t row_r, ssize_t col_l, ssize_t col_r,
-			  int val) {
+static struct cell search_matrix_aux(size_t M, size_t N, int matrix[M][N],
+				     ssize_t row_l, ssize_t row_r, ssize_t col_l, ssize_t col_r,
+				     int val) {
+
 	struct cell res = { -1, -1 };
+
 	if (row_l > row_r || col_l > col_r) {
 		return res;
 	}
-
-	if (row_l == row_r && col_l == col_r) {
-		if (matrix[row_l][col_l] == val) {
-			res.row = row_l;
-			res.col = col_l;
+    
+	int mid_x = row_l+(row_r-row_l)/2;
+	int mid_y = col_l+(col_r-col_l)/2;
+    
+	if (matrix[mid_x][mid_y] == val) {
+		res.row = mid_x;
+		res.col = mid_y;
+	} else if (matrix[mid_x][mid_y] < val) {
+		res = search_matrix_aux(M, N, matrix, mid_x+1, row_r, col_l, col_r, val);
+		if (res.row == -1) {
+			res = search_matrix_aux(M, N, matrix, row_l, mid_x, mid_y+1, col_r, val);
 		}
-		return res;
-	}
-
-	ssize_t diag_x = row_l+(row_r-row_l)/2;
-	ssize_t diag_y = col_l+(col_r-col_l)/2;
-
-	if (matrix[diag_x][diag_y] == val) {
-		res.row = diag_x;
-		res.col = diag_y;
-		return res;
-	} else if (val < matrix[diag_x][diag_y]) {
-		res = bin_search_2d_aux(M, N, matrix, row_l, diag_x, col_l, diag_y, val);
-	} else if (diag_x+1 <= row_r && diag_y+1 <= col_r && matrix[diag_x+1][diag_y+1] == val) {
-		res.row = diag_x+1;
-		res.col = diag_y+1;
-		return res;
-	} else if (diag_x+1 <= row_r && diag_y+1 <= col_r && val > matrix[diag_x+1][diag_y+1]) {
-		res = bin_search_2d_aux(M, N, matrix, diag_x+1, row_r, diag_y+1, col_r, val);
-	}
-
-
-	if (res.row == -1) {
-		res = bin_search_2d_aux(M, N, matrix, diag_x+1, row_r, col_l, diag_y, val);
-	}
-	if (res.row == -1) {
-		res = bin_search_2d_aux(M, N, matrix, row_l, diag_x, diag_y+1, col_r, val);
+	} else {
+		res = search_matrix_aux(M, N, matrix, row_l, mid_x-1, mid_y, col_r, val);
+		if (res.row == -1) {
+			res = search_matrix_aux(M, N, matrix, row_l, row_r, col_l, mid_y-1, val);
+		}
 	}
 
 	return res;
 }
 
 struct cell bin_search_2d(size_t M, size_t N, int matrix[M][N], int val) {
-	return bin_search_2d_aux(M, N, matrix, 0, M-1, 0, N-1, val);
+	return search_matrix_aux(M, N, matrix, 0, M-1, 0, N-1, val);
 }
 
 int main(void) {
