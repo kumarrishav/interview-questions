0796d60ccb885683966c86f7e16cbc5ed36c6f53
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-25 22:03:44 +0100

    Solved rod cutting problem

---
+++ recursion_dp/rod_cutting/solution.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <assert.h>
+
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+
+int cut_rod(int prices[], ssize_t rod_len) {
+	if (rod_len <= 0) {
+		return 0;
+	}
+
+	int res = prices[0]+cut_rod(prices, rod_len-1);
+	ssize_t i;
+	for (i = 1; i < rod_len; i++) {
+		res = max(res, prices[i]+cut_rod(prices, rod_len-i-1));
+	}
+
+	return res;
+}
+
+static int prices_buff[1024];
+
+int main(void) {
+	printf("Enter size of rod followed by the prices array.\n");
+	printf("> ");
+
+	size_t rod_len;
+
+	while (scanf("%zu", &rod_len) == 1) {
+		size_t i;
+		for (i = 0; i < rod_len; i++) {
+			scanf("%d", &prices_buff[i]);
+		}
+
+		printf("Best value = %d\n", cut_rod(prices_buff, rod_len));
+		printf("> ");
+	}
+
+	return 0;
+}
