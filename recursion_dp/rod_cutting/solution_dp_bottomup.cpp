92515bb9d275e63cee325dab1682c950e7167210
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-25 22:04:08 +0100

    Solved rod cutting problem with bottom up DP

---
+++ recursion_dp/rod_cutting/solution_dp_bottomup.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int>::size_type size_type;
+
+int cut_rod(const vector<int> &prices) {
+	vector<int> dp(prices.size()+1);
+
+	dp[0] = 0;
+
+	for (size_type l = 1; l <= prices.size(); l++) {
+		dp[l] = dp[0]+prices[l-1];
+		for (size_type i = 1; i < l; i++) {
+			dp[l] = max(dp[l], dp[i]+prices[l-i-1]);
+		}
+	}
+
+	return dp[prices.size()];
+}
+
+int main(void) {
+	cout << "Enter size of rod followed by the prices array." << endl;
+	cout << "> ";
+
+	size_type rod_len;
+
+	while (cin >> rod_len) {
+		vector<int> prices(rod_len);
+
+		for (size_type i = 0; i < rod_len; i++) {
+			cin >> prices[i];
+		}
+
+		cout << "Best value = " << cut_rod(prices) << endl;
+		cout << "> ";
+	}
+
+	return 0;
+}
