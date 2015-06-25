b3938c32f3f1d103a3ead20e3a3217520b686a4b
Filipe Gonçalves <filipe@codinghighway.com>
2015-06-25 16:16:47 +0100

    Solved "three repeats"

---
+++ bit_manipulation/three_repeats/solution.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <assert.h>
+
+int find_number(int arr[], size_t n) {
+	size_t i;
+	int ones, twos, mask;
+
+	ones = twos = 0;
+	for (i = 0; i < n; i++) {
+		mask = arr[i] & ~twos;
+		ones ^= mask;
+		twos ^= arr[i] & ~mask;
+		mask = (mask & ones) ^ mask;
+		twos ^= mask;
+		assert((ones & twos) == 0);
+	}
+
+	assert(ones == 0 || twos == 0);
+
+	return twos == 0 ? ones : twos;
+}
+
+static int arr_buff[1024];
+
+int main(void) {
+	printf("Enter size of array, followed by elements. Every element must be repeated 3 times except for one of them.\n");
+	printf("> ");
+
+	size_t n;
+	while (scanf("%zu", &n) == 1) {
+		size_t i;
+		for (i = 0; i < n; i++) {
+			scanf("%d", &arr_buff[i]);
+		}
+
+		printf("%d\n", find_number(arr_buff, n));
+		printf("> ");
+	}
+
+	return 0;
+}
