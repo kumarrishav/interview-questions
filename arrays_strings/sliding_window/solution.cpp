ad17b05aa3ec3c1dda57f9a15d000a17a6858a47
Filipe Goncalves <filipe@codinghighway.com>
2015-07-03 16:28:59 +0100

    Updated "sliding window" description

--- arrays_strings/sliding_window/solution.cpp
+++ arrays_strings/sliding_window/solution.cpp
@@ -1,10 +1,26 @@
-
+/* You have a stream of integers where you can only access the next integer.
+ *
+ * Write a function that behaves as if a window of size K slides through this
+ * stream. For each slide, print the maximum value inside the window.
+ *
+ * EXAMPLE
+ *
+ * K = 4
+ * Stream = 21, 10, 2, 11, 9, 5, 6, 5, 7, 5, 3
+ *
+ * Output: 21 11 11 11 9 7 7 7
+ *
+ * Source: Careercup (Microsoft interview)
+ */
 #include <iostream>
 #include <deque>
 #include <vector>
 
 using namespace std;
 
+/* Note: this amortizes to O(N), where N is the size of the stream, because each
+ * element is pushed and popped from the queue at most twice.
+ */
 vector<int> max_sliding_window(const vector<int> &stream, vector<int>::size_type window_sz) {
 	vector<int> res;
 	deque<vector<int>::size_type > maxs;
