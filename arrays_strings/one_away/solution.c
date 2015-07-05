80b3e08fe2ae6a82d4c2abf2e47a36b47fc382ac
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-05 19:54:55 +0100

    Solved "one away"

---
+++ arrays_strings/one_away/solution.c
@@ -0,0 +1,60 @@
+
+#include <stdio.h>
+#include <string.h>
+
+size_t compare_strings(const char *str1, const char *str2) {
+	size_t i;
+	for (i = 0; str1[i] != '\0' && str1[i] == str2[i]; i++)
+		; /* Intentionally left blank */
+	return i;
+}
+
+int one_away(const char *str1, const char *str2) {
+	const char *largest_str = str1;
+	size_t largest_len = strlen(str1);
+
+	const char *smallest_str = str2;
+	size_t smallest_len = strlen(str2);
+
+	if (largest_len < smallest_len) {
+		size_t tmp = smallest_len;
+		smallest_len = largest_len;
+		largest_len = tmp;
+		largest_str = str2;
+		smallest_str = str1;
+	}
+
+	if (largest_len > smallest_len+1) {
+		return 0;
+	}
+
+	size_t i = compare_strings(largest_str, smallest_str);
+	if (largest_str[i] == '\0') {
+		return 1;
+	}
+
+	if (largest_len == smallest_len) {
+		return compare_strings(largest_str+i+1, smallest_str+i+1)+i+1 == largest_len;
+	} else {
+		return compare_strings(largest_str+i+1, smallest_str+i)+i+1 == largest_len;
+	}	
+}
+
+static char str1[1024];
+static char str2[1024];
+
+int main(void) {
+	printf("Enter two strings\n");
+	printf("> ");
+
+	while (scanf("%s%s", str1, str2) == 2) {
+		if (one_away(str1, str2)) {
+			printf("Yes.\n");
+		} else {
+			printf("No.\n");
+		}
+		printf("> ");
+	}
+
+	return 0;
+}
