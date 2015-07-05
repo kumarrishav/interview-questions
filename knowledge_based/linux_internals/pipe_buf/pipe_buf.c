e0ad3bae5cc58118be2c5914e4a3e7619b466a81
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-05 15:50:41 +0100

    Updated pipe capacity tests description

--- knowledge_based/linux_internals/pipe_buf/pipe_buf.c
+++ knowledge_based/linux_internals/pipe_buf/pipe_buf.c
@@ -1,10 +1,26 @@
-
+/* Determine the capacity of a pipe using nonblocking writes. Compare this value with the
+ * value of PIPE_BUF from Chapter 2.
+ *
+ * Source: Advanced Programming in the UNIX Environment, 3rd Ed., Chapter 14, Exercise 14.7
+ */
 #include <limits.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
+
+/* This test creates a pipe, sets the write end non blocking, and writes into the pipe until it
+ * is not possible to write anymore without blocking.
+ *
+ * In Linux, the pipe capacity is 65536 bytes. This is the amount of bytes successfully written
+ * before write(2) returns EAGAIN or EWOULDBLOCK.
+ *
+ * In constrast, PIPE_BUF is 4096, a much smaller value. Note that PIPE_BUF is the largest value
+ * that is atomically written into the pipe. This is not necessarily the pipe capacity, which is
+ * generally greater, as can be proven by this example.
+ *
+ */
 
 int main(void) {
 	int fildes[2];
