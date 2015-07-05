043d33664dd07eefbf6c50af6a5cc67417b8ec9c
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-05 19:56:32 +0100

    Updated "one away" description

--- arrays_strings/one_away/solution.c
+++ arrays_strings/one_away/solution.c
@@ -1,4 +1,16 @@
-
+/* There are three types of edits that can be performed on strings: insert a character,
+ * remove a character, or replace a character. Given two strings, write a function to check if they
+ * are one edit (or zero edits) away.
+ *
+ * EXAMPLE
+ *
+ * pale, ple -> true
+ * pales, pale -> true
+ * pale, bale -> true
+ * pale, bake -> false
+ *
+ * Source: Cracking the Coding Interview, 6th Ed., page 91, exercise 1.5
+ */
 #include <stdio.h>
 #include <string.h>
 
