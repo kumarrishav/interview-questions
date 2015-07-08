36c9c71a994cf69f314d3b6667f8a87b723edfd2
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-08 16:57:57 +0100

    Updated "random node" description

--- trees_graphs/random_node/solution.c
+++ trees_graphs/random_node/solution.c
@@ -1,4 +1,10 @@
-
+/* You are implementing a binary tree class from scratch which, in addition to insert, find and
+ * delete, has a method getRandomNode() which returns a random node from the tree. All nodes
+ * should be equally likely to be chosen. Design and implement an algorithm for getRandomNode(),
+ * and explain how you would implement the rest of the methods.
+ *
+ * Source: Cracking the Coding Interview, 6th Edition, Exercise 4.11 (page 111)
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
