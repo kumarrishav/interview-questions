8818eb2b3bb1de401adb14b5d47a52ffee56d300
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-09 20:12:28 +0100

    Added explanation to "sort attempt"

--- trees_graphs/sort_attempt/solution.cpp
+++ trees_graphs/sort_attempt/solution.cpp
@@ -9,6 +9,9 @@
  * must return No as an answer. In other words, only return Yes if the given predicates lead to a
  * unique sort order.
  *
+ * Assume that the set of predicates is not necessarily consistent: we may see (ai, aj) and (aj, ai)
+ * in our predicates list. If this happens, the algorithm should also return No.
+ *
  * EXAMPLES
  *
  * Consider the list [ 2, 1, 3 ] and the predicates:
@@ -39,6 +42,49 @@
 #include <iostream>
 
 using namespace std;
+
+/* This deserves a nice explanation.
+ *
+ * We solve this problem with the help of graphs and topological sort. The vertices in the graph
+ * correspond to the list elements. There is an edge from V to W if V is known to be greater than W.
+ * With this arrangement, the problem is reduced to determining whether the graph doesn't have a
+ * cycle and there is only one topological sort.
+ *
+ * So, the algorithm does these two steps: first, it determines whether there is a cycle. It does so
+ * by doing a regular DFS, keeping track of which nodes are currently on the stack (using the
+ * on_stack flag in the node's struct). If the recursive call is entered with a node that is already
+ * in the recursion stack, then we know that we found a back edge in the DFS tree, so there is a
+ * cycle. The algorithm returns No prematurely.
+ *
+ * Note that being on the stack is not the same as being visited; the flags have different meanings.
+ * The visited flag indicates whether the node has been reached in the current DFS traversal as a
+ * whole. If the graph has islands, or if we start the DFS traversal in a node in the middle of the
+ * graph, we might have to later start other DFS searches from other nodes that we couldn't reach,
+ * and perhaps we will reach visited nodes again, but that does not necessarily mean that there is a
+ * cycle.
+ *
+ * Consider for example the special case of a tree - if we start a DFS traversal from a node in the
+ * middle, we would never reach nodes above, and then we would have to start other DFS traversals
+ * from those nodes above in the next iterations and we would eventually hit the nodes below that
+ * were already visited.
+ * So, the bottom line is that there isn't a cycle unless we reach a node that is in the current
+ * DFS recursion stack. The flags are similar, but they have slightly different semantics.
+ * This is very easy to overlook in an interview, yet it's relatively simple to explain and fix,
+ * and it will definitely make a good impression.
+ *
+ * If there is no cycle, we run a modified topological sort that knows how to ensure uniqueness
+ * along the way. A topological sort is unique if each pair of consecutive vertices in the sorted
+ * result is connected by an edge (in which case the edges form a directed Hamiltonian path in the
+ * DAG). So, we implement a modified topological sort that keeps track of the previous element that
+ * would have been inserted in the result set. Then, when the time comes to insert the next element,
+ * we check that element against the last inserted element. If there isn't an edge connecting them,
+ * then there is more than one possible topological sort and the algorithm returns No.
+ *
+ * Note that we don't actually have to store the result of topological sort, we just check along the
+ * way if each pair of adjacent vertices in the result set would have been connected by an edge.
+ * Pretty elegant, huh?
+ *
+ */
 
 struct graph_node {
 	int id;
