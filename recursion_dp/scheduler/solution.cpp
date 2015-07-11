859139d606b83ac29f6f3ad7e9d51b81df2a1d40
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-12 00:35:56 +0100

    Solved "scheduler"

---
+++ recursion_dp/scheduler/solution.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+unsigned scheduler_possibilities(unsigned m, unsigned n) {
+	if (m == 0 || n == 0) {
+		return 1;
+	}
+	return scheduler_possibilities(m-1, n)+scheduler_possibilities(m, n-1);
+}
+
+unsigned scheduler_possibilities_dp(unsigned m, unsigned n) {
+	unsigned dim = max(m+1, n+1);
+	vector<vector<unsigned> > dp(dim, vector<unsigned>(dim));
+
+	for (size_t i = 0; i < dim; i++) {
+		dp[i][0] = 1;
+		dp[0][i] = 1;
+	}
+
+	for (size_t i = 1; i < dim; i++) {
+		for (size_t j = 1; j < dim; j++) {
+			dp[i][j] = dp[i-1][j]+dp[i][j-1];
+		}
+	}
+
+	return dp[m][n];
+}
+
+int main(void) {
+	cout << "Enter M and N" << endl;
+	cout << "> ";
+
+	unsigned m, n;
+	while (cin >> m >> n) {
+		// naive recursion will take forever with big values
+		// cout << scheduler_possibilities(m, n) << endl;
+		cout << scheduler_possibilities_dp(m, n) << endl << "> ";
+	}
+
+	return 0;
+}
