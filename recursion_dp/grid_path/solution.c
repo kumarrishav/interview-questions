9ac198ff8838b9a2dad4fa25f8de9222c2eb8927
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-12 15:49:13 +0100

    Reimplemented "grid path" using bottom-up DP

--- recursion_dp/grid_path/solution.c
+++ recursion_dp/grid_path/solution.c
@@ -4,9 +4,12 @@
  *
  * Source: Cracking the Coding Interview, page 109, question 9.2
  */
-#include <stdio.h>
 
-/* Recursive approach */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/* Naive recursion */
 unsigned long long paths_aux(size_t curr_x, size_t curr_y, size_t x, size_t y) {
 	unsigned long long res = 0;
 
@@ -29,40 +32,45 @@ unsigned long long paths_rec(size_t x, size_t y) {
 	return paths_aux(0, 0, x, y);
 }
 
-/* Maths approach */
-unsigned long long factorial(unsigned n);
-unsigned long long binomial(unsigned n, unsigned r);
-unsigned long long paths_math(size_t x, size_t y) {
-	return binomial(x+y, x);
-}
+/* DP solution */
+unsigned long long paths_dp(size_t x, size_t y) {
+	unsigned long long **dp = malloc(sizeof(*dp)*(x+1));
+	assert(dp != NULL);
 
-unsigned long long factorial(unsigned n) {
-	unsigned long long res = 1;
-
-	while (n > 1) {
-		res *= n;
-		n--;
+	size_t i;
+	for (i = 0; i < x+1; i++) {
+		dp[i] = malloc(sizeof(*dp[i])*(y+1));
+		assert(dp[i] != NULL);
 	}
 
-	return res;
-}
+	for (i = 0; i < x+1; i++)
+		dp[i][0] = 1;
+	for (i = 0; i < y+1; i++)
+		dp[0][i] = 1;
 
-unsigned long long binomial(unsigned n, unsigned r) {
-	return factorial(n)/(factorial(r)*factorial(n-r));
+	size_t j;
+	for (i = 1; i < x+1; i++)
+		for (j = 1; j < y+1; j++)
+			dp[i][j] = dp[i][j-1]+dp[i-1][j];
+
+	unsigned long long ret = dp[x][y];
+
+	for (i = 0; i < x+1; i++)
+		free(dp[i]);
+	free(dp);
+
+	return ret;
 }
 
 int main(void) {
 	size_t x, y;
 
-	printf("Enter the values for X and Y\n");
+	printf("Enter the values for X and Y\n> ");
 
 	while (scanf("%zu%zu", &x, &y) == 2) {
 
-		unsigned long long rec = paths_rec(x, y);
-		unsigned long long maths = paths_math(x, y);
-
-		printf("Recursive: %llu\n", rec);
-		printf("Maths: %llu\n", maths);
+		// printf("Naive recursion: %llu\n", paths_rec(x, y));
+		printf("DP: %llu\n> ", paths_dp(x, y));
 	}
 
 	return 0;
