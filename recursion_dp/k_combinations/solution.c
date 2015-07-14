35e7e67c5d19925f2576c4f4c025c70f4c7939fc
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-14 21:54:55 +0100

    Solved "k combinations"

---
+++ recursion_dp/k_combinations/solution.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <assert.h>
+
+static char out_buff[1024];
+static size_t buff_i;
+
+void k_combinations_aux(int arr[], size_t arr_len, size_t curr_i, size_t k) {
+	if (k == 0) {
+		out_buff[buff_i] = '\0';
+		printf("{ %s}\n", out_buff);
+		return;
+	}
+
+	if (curr_i == arr_len) {
+		return;
+	}
+
+	if (arr_len-curr_i > k) {
+		k_combinations_aux(arr, arr_len, curr_i+1, k);
+	}
+
+	int added = sprintf(&out_buff[buff_i], "%d ", arr[curr_i]);
+	buff_i += added;
+	k_combinations_aux(arr, arr_len, curr_i+1, k-1);
+	buff_i -= added;
+}
+
+void k_combinations(int arr[], size_t arr_len, size_t k) {
+	buff_i = 0;
+	k_combinations_aux(arr, arr_len, 0, k);
+}
+
+static int arr[1024];
+
+int main(void) {
+	printf("Enter array size, followed by the elements, followed by K\n");
+	printf("> ");
+
+	size_t arr_sz;
+	while (scanf("%zu", &arr_sz) == 1) {
+		assert(arr_sz < sizeof(arr)/sizeof(arr[0]));
+		size_t i;
+		for (i = 0; i < arr_sz; i++) {
+			scanf("%d", &arr[i]);
+		}
+		size_t k;
+		scanf("%zu", &k);
+		k_combinations(arr, arr_sz, k);
+		printf("> ");
+	}
+
+	return 0;
+}
