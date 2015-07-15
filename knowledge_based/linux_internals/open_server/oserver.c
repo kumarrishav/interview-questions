8bbbd05e8e8c7c483bb3aecfb082d708682ada0c
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-15 17:03:11 +0100

    send_errno() sends credentials

--- knowledge_based/linux_internals/open_server/oserver.c
+++ knowledge_based/linux_internals/open_server/oserver.c
@@ -54,13 +54,42 @@ int send_fd(int sockfd, int fd_to_send) {
 
 /* TODO Update errno to send credentials */
 int send_errno(int sockfd) {
-	ssize_t sent = send(sockfd, &errno, sizeof(errno), 0);
+
+	struct iovec iov;
+	iov.iov_base = &errno;
+	iov.iov_len = sizeof(errno);
+
+	unsigned char cmsg_buff[CMSG_SPACE(sizeof(struct ucred))];
+
+	struct msghdr msg;
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = cmsg_buff;
+	msg.msg_controllen = sizeof(cmsg_buff);
+	msg.msg_flags = 0;
+
+	struct cmsghdr *cmptr = CMSG_FIRSTHDR(&msg);
+	cmptr->cmsg_len = CMSG_LEN(sizeof(struct ucred));
+	cmptr->cmsg_level = SOL_SOCKET;
+	cmptr->cmsg_type = SCM_CREDENTIALS;
+
+	struct ucred *cred_ptr = (struct ucred *) CMSG_DATA(cmptr);
+	cred_ptr->pid = getpid();
+	cred_ptr->uid = geteuid();
+	cred_ptr->gid = getegid();
+
+	ssize_t sent = sendmsg(sockfd, &msg, 0);
+
 	if (sent < 0)
 		return -1;
+
 	if (sent != sizeof(errno)) {
 		errno = ECOMM;
 		return -1;
 	}
+
 	return 0;
 }
 
