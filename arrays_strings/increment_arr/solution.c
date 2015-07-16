1916aecc3379064b23a808ee395f940119e69973
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-16 20:56:45 +0100

    Solved "increment array"

---
+++ arrays_strings/increment_arr/solution.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+
+int incr_array(int arr[], size_t arr_sz) {
+	ssize_t i = arr_sz;
+	i--;
+
+	while (i >= 0 && arr[i] == 9) {
+		arr[i] = 0;
+		i--;
+	}
+
+	if (i >= 0) {
+		arr[i]++;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+static void print_array(int arr_buff[], size_t arr_sz) {
+	printf("[ ");
+
+	if (arr_sz > 0) {
+		printf("%d", arr_buff[0]);
+	}
+
+	size_t i;
+	for (i = 1; i < arr_sz; i++) {
+		printf(", %d", arr_buff[i]);
+	}
+
+	printf(" ]\n");
+}
+
+static int arr_buff[1024];
+
+int main(void) {
+	printf("Enter the size of the array, followed by its elements\n");
+	printf("> ");
+
+	size_t arr_sz;
+	while (scanf("%zu", &arr_sz) == 1) {
+		size_t i;
+		for (i = 0; i < arr_sz; i++) {
+			scanf("%d", &arr_buff[i]);
+		}
+		print_array(arr_buff, arr_sz);
+		while (incr_array(arr_buff, arr_sz) == 1) {
+			print_array(arr_buff, arr_sz);
+		}
+		printf("> ");
+	}
+
+	return 0;
+}
