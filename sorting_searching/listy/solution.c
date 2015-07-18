3ad97a35f82d305783a50d76756ed7c8502cbc32
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-18 19:51:28 +0100

    Updated "listy" description

--- sorting_searching/listy/solution.c
+++ sorting_searching/listy/solution.c
@@ -1,3 +1,11 @@
+/* You are given an array-like data structure Listy which lacks a size method. It does, however,
+ * have an elementAt(i) method that returns the element at index i in O(1) time. If i is beyond the
+ * bounds of the data structure, it returns -1. (For this reason, the data structure only supports
+ * positive integers). Given a Listy which contains sorted, positive integers, find the index at
+ * which an element x occurs. If x occurs multiple times, you may return any index.
+ *
+ * Source: Cracking the Coding Interview, 6th Ed., Exercise 10.4 (page 150)
+ */
 
 #include <stdio.h>
 
