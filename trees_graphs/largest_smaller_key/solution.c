00cbdab5a110a64f102324bc7164b2ad436e2d94
Filipe Gonçalves <filipe@codinghighway.com>
2015-07-18 17:34:21 +0100

    Updated "largest smaller key" description

--- trees_graphs/largest_smaller_key/solution.c
+++ trees_graphs/largest_smaller_key/solution.c
@@ -1,3 +1,12 @@
+/* Given the root of a binary search tree and a key x, find the largest key in the tree that is
+ * smaller than x.
+ *
+ * EXAMPLE
+ *
+ * If an in-order traversal of all keys in the tree is [ 2, 3, 4, 7, 17, 19, 21, 35, 89 ] and
+ * x is 19, the biggest key that is smaller than x is 17.
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
