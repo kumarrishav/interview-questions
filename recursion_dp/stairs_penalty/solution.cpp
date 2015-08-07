26768686e28a0ee04c36a0f176500a6e867cfebd
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-07 12:24:18 +0100

    Updated "stairs penalty" description

--- recursion_dp/stairs_penalty/solution.cpp
+++ recursion_dp/stairs_penalty/solution.cpp
@@ -1,3 +1,28 @@
+/* You have a pair of stairs, one is on your left, the other is on your right.
+ * Both stairs have the same number of steps N.
+ *
+ * Your goal is to reach the end of either stairs (that is, reach step N of one of the stairs).
+ *
+ * There is a cost for going up one step on each stair. It is also possible to jump from one stair
+ * to the other, but the jumps are horizontal, i.e., if you are on step i of the left stair and you
+ * jump, then you will be on step i of the right stair. Each jump has a cost too.
+ *
+ * The costs of going up one step depend on the stair (left or right) and where in that stair you
+ * are.
+ *
+ * The cost of horizontal jumps to switch stairs depends only on the step level.
+ *
+ * You are given 3 arrays with these costs:
+ *
+ * A[0..N-1], where A[i] is the cost of jumping to the other stair at step i.
+ * B[0..N-2], where B[i] is the cost of going from step i to step i+1 on the left stair.
+ * C[0..N-1], where C[i] is the cost of going from step i to step i+1 on the right stair.
+ *
+ * Write code that finds the minimum cost to reach the end of one of the stairs. You can start
+ * on the first step of any stair. You can only go up one step at a time.
+ *
+ * Source: Careercup (Microsoft interview)
+ */
 
 #include <iostream>
 #include <algorithm>
@@ -5,6 +30,7 @@
 
 using namespace std;
 
+/* Brute force recursive solution. O(2^N) */
 static unsigned min_penalty(const vector<unsigned> &jump_p, const vector<unsigned> steps[2],
 			    unsigned stair, vector<unsigned>::size_type stair_i) {
 	if (stair_i == steps[0].size())
@@ -24,6 +50,7 @@ unsigned min_penalty_brute_force(const vector<unsigned> &jump_p, const vector<un
 	return min(min_penalty(jump_p, steps, 0, 0), min_penalty(jump_p, steps, 1, 0));
 }
 
+/* Dynamic programming (bottom-up) solution. Way more elegant. O(n) time and memory */
 unsigned min_penalty_dp(const vector<unsigned> &jump_p, const vector<unsigned> steps[2]) {
 
 	vector<vector<unsigned> > dp_table(2, vector<unsigned>(steps[0].size()+1));
@@ -75,6 +102,13 @@ int main(void) {
 			steps[1].push_back(v);
 		}
 
+		/* Uncomment the next line to compare results. This was used to validate the
+		 * correctness of the dynamic programming solution.
+		 *
+		 * The brute force solution does not terminate timely for big inputs because
+		 * it has exponential running time.
+		 */
+ 
 		// cout << "Brute force: " << min_penalty_brute_force(jumps, steps) << endl;
 		cout << "DP: " << min_penalty_dp(jumps, steps) << endl;
 
