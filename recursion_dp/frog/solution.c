79dda1065f2c414f130f691614f03e360d42f8f3
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-25 13:27:15 +0100

    Updated "frog" description

--- recursion_dp/frog/solution.c
+++ recursion_dp/frog/solution.c
@@ -1,6 +1,22 @@
+/* A frog only moves forward, but it can move in steps 1 inch long or in jumps 2 inches long. A frog
+ * can cover the same distance using different combinations of steps and jumps.
+ *
+ * Write a function that calculates the number of different combinations a frog can use to cover a
+ * given distance.
+ *
+ * For example, a distance of 3 inches can be covered in three ways: step-step-step, step-jump,
+ * and jump-step.
+ *
+ * Source: StackOverflow
+ */
 
 #include <stdio.h>
 
+/* This problem is reduced to the problem of finding the N-th Fibonacci number, which can be solved
+ * in O(N) time and O(1) memory
+ *
+ * See http://stackoverflow.com/questions/32202911
+ */
 int frog_combos(int n) {
 	int before_last = 1, last = 0;
 	int i;
