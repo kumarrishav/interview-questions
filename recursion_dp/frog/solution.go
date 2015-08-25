41e6a0278808a6fa047682018108a5c6567d8eed
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-25 13:29:01 +0100

    Updated "frog" description in the Go solution

--- recursion_dp/frog/solution.go
+++ recursion_dp/frog/solution.go
@@ -1,8 +1,26 @@
+/* A frog only moves forward, but it can move in steps 1 inch long or in jumps 2 inches long. A frog
+ * can cover the same distance using different combinations of steps and jumps.
+ *
+ * Write a function that calculates the number of different combinations a frog can use to cover a
+ * given distance.
+ *
+ * For example, a distance of 3 inches can be covered in three ways: step-step-step, step-jump,
+ * and jump-step.
+ *
+ * Source: StackOverflow
+ */
 
 package main
 
+/* This is a nice solution in Go, as an alternative to C. */
+
 import "fmt"
 
+/* This problem is reduced to the problem of finding the N-th Fibonacci number, which can be solved
+ * in O(N) time and O(1) memory
+ *
+ * See http://stackoverflow.com/questions/32202911
+ */
 func fib() func() int {
 	before_last, last := 1, 0
 	return func() int {
