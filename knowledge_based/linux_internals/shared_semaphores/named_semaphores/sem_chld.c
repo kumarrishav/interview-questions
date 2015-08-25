ce56630ca01139d6ca49ce4f17e78c04f6656da7
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-25 16:27:17 +0100

    Added named semaphores example

---
+++ knowledge_based/linux_internals/shared_semaphores/named_semaphores/sem_chld.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define SEM_NAME "/semaphore_example"
+#define ITERS 10
+
+int main(void) {
+	sem_t *semaphore = sem_open(SEM_NAME, O_RDWR);
+	if (semaphore == SEM_FAILED) {
+		perror("sem_open(3) failed");
+		exit(EXIT_FAILURE);
+	}
+
+	int i;
+	for (i = 0; i < ITERS; i++) {
+		if (sem_wait(semaphore) < 0) {
+			perror("sem_wait(3) failed on child");
+			continue;
+		}
+
+		printf("PID %ld acquired semaphore\n", (long) getpid());
+
+		if (sem_post(semaphore) < 0) {
+			perror("sem_post(3) error on child");
+		}
+
+		sleep(1);
+	}
+
+	if (sem_close(semaphore) < 0)
+		perror("sem_close(3) failed");
+
+	return 0;
+}
